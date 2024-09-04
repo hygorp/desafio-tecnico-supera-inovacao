49928fb77487d1683b2e660b162d13f62da4f2ec
hygorp <hygor.henrique@live.com>
2024-09-04 19:30:29 -0300

    Inicializando o desenvolvimento

---
+++ src/test/java/org/hygorp/listmanager/ListManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hygorp.listmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
